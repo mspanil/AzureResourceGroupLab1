6515a6f784c701d13659fc160061a668fd1d5095
mspanil <88969017+mspanil@users.noreply.github.com>
2021-09-30 16:33:48 +0530

    Add files via upload

--- AzureResourceGroup1/Deploy-AzureResourceGroup.ps1
+++ AzureResourceGroup1/Deploy-AzureResourceGroup.ps1
@@ -2,7 +2,7 @@
 
 Param(
     [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
-    [string] $ResourceGroupName = 'AzureResourceGroupLab1',
+    [string] $ResourceGroupName = 'PribitautoresourceGroup',
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
     [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
